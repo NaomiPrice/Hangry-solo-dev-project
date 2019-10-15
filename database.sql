79d9276a147fe2b8f61ec640baefbe830281a0ac
Naomi Price <naomi.price71@gmail.com>
2019-10-15 09:51:27 -0500

    set up database tables

--- database.sql
+++ database.sql
@@ -6,5 +6,31 @@
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "firstName" VARCHAR (50)
+);
+
+CREATE TABLE "collections" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (150) NOT NULL,
+    "user_id" INT REFERENCES "user"
+);
+
+CREATE TABLE "restaurants" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR (150) NOT NULL,
+    "collection_id" INT REFERENCES "collections",
+    "user_id" INT REFERENCES "user",
+    "google_places_id" VARCHAR (150)
+);
+
+CREATE TABLE "notes" (
+    "id" SERIAL PRIMARY KEY,
+    "notes_field" VARCHAR (500) NOT NULL,
+    "user_id" INT REFERENCES "user",
+    "restaurant_id" INT REFERENCES "restaurants",
+    "date_time_created" TIMESTAMP NOT NULL 
+    			DEFAULT CURRENT_TIMESTAMP,
+    "date_time_modified" TIMESTAMP NOT NULL
+    			DEFAULT CURRENT_TIMESTAMP        
 );
