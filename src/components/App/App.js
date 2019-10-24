a076d03e04653a133f7c145dee740f42901c25fd
Naomi Price <naomi.price71@gmail.com>
2019-10-24 16:19:57 -0500

    styling - updating color to rose

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,7 +13,6 @@ import Footer from '../Footer/Footer';
 
 import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 
-import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import CollectionPage from '../CollectionPage/CollectionPage';
 import RestaurantPage from '../RestaurantPage/RestaurantPage';
@@ -41,13 +40,8 @@ class App extends Component {
           <Switch>
             {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
             <Redirect exact from="/" to="/home" />
-            {/* Visiting localhost:3000/about will show the about page.
-            This is a route anyone can see, no login necessary */}
-            <Route
-              exact
-              path="/about"
-              component={AboutPage}
-            />
+           
+            
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
             If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
